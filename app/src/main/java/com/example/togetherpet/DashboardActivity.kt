8dea1b293e5c532d4457df8b0fbddf66d3af3b68
fiveJinw <fiveJinw1124@gmail.com>
2024-09-23 01:06:05 +0900

    style : DashboardActivity 오타 수정

--- app/src/main/java/com/example/togetherpet/DashboardActivity.kt
+++ app/src/main/java/com/example/togetherpet/DashboardActivity.kt
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class qwqDashboardActivity : AppCompatActivity() {
+class DashboardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
