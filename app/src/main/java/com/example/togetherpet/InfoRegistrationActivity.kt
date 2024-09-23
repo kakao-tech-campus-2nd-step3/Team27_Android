8139d03c1c4b4fe73e0cf7eb19ff5a18961112a0
fiveJinw <fiveJinw1124@gmail.com>
2024-09-24 01:26:16 +0900

    style : MainActivity, xml 이름 변경

--- app/src/main/java/com/example/togetherpet/InfoRegistrationActivity.kt
+++ app/src/main/java/com/example/togetherpet/InfoRegistrationActivity.kt
@@ -2,16 +2,13 @@ package com.example.togetherpet
 
 import android.os.Bundle
 import android.util.Log
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.kakao.sdk.common.util.Utility
 
-class MainActivity : AppCompatActivity() {
+class InfoRegistrationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_info_registration)
         Log.d("testt", Utility.getKeyHash(this))
 
 
