c6636388f3de6ddc97a9bf30d6592c692a17a913
fiveJinw <fiveJinw1124@gmail.com>
2024-10-01 15:46:41 +0900

    feat : 산책 화면 xml 추가

--- app/src/main/java/com/example/togetherpet/fragment/WalkingPetFragment.kt
+++ app/src/main/java/com/example/togetherpet/fragment/WalkingPetFragment.kt
@@ -1,11 +1,16 @@
 package com.example.togetherpet.fragment
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.togetherpet.databinding.FragmentWalkingPetBinding
+import com.kakao.vectormap.KakaoMap
+import com.kakao.vectormap.KakaoMapReadyCallback
+import com.kakao.vectormap.MapLifeCycleCallback
+import com.kakao.vectormap.MapView
 
 class WalkingPetFragment : Fragment() {
     private var _binding: FragmentWalkingPetBinding? = null
@@ -17,6 +22,24 @@ class WalkingPetFragment : Fragment() {
     ): View {
         _binding = FragmentWalkingPetBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val map = binding.walkingMapView
+        map.start(object : MapLifeCycleCallback() {
+            override fun onMapDestroy() {
+                Log.d("testt", "MapDestroy")
+            }
+
+            override fun onMapError(error: Exception) {
+                //에러 처리
+            }
+        }, object : KakaoMapReadyCallback() {
+            override fun onMapReady(kakaoMap: KakaoMap) {
+                Log.d("testt", "MapReady")
+            }
+        })
     }
 
     override fun onDestroyView() {
