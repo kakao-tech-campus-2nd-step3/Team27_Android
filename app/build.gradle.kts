07b5aac490de1aa580c1aa9832911727aedf9bd8
fiveJinw <131530830+fiveJinw@users.noreply.github.com>
2024-10-04 23:14:51 +0900

    [Week5] 홈 화면 구현 및 산책 기능 일부 구현 (#30)
    * feat : 앱 반려동물 등록 시작화면 xml 추가
    
    * feat : 반려동물 정보 등록창 xml 생성
    
    * design: Dashboard 화면에 BottomNavigation 추가
    
    - 화면 전환은 추후에 구현
    
    * chore: .idea 폴더 삭제
    
    chore: .idea 폴더 삭제
    
    body
    .idea 폴더는 intellij 세팅 폴더로 삭제
    
    * docs : gitignore 수정 및 템플릿 추가
    
    * docs: 3주차 리뷰 받고 싶은 내용 정리
    
    * docs: README 오타 수정
    
    * chore: Gradle Migration 버전 1로 설정
    
    - 자동 생성된 코드
    
    * design: BottomNavigationView에서 아이콘 클릭 시 발생하는 배경 제거
    
    - 피드백 반영 (https://github.com/kakao-tech-campus-2nd-step3/Team27_Android/pull/9)
    - BottomNavigationView의 색상이 Emulator에서 투명도가 있는 흰색으로 보여 확인이 필요함
    
    * fix: 원격 브랜치의 변경 사항 반영
    
    - 코드 수정 전 원격 브랜치의 변경 사항을 반영하지 않아 PR에서 충돌 발생 (https://github.com/kakao-tech-campus-2nd-step3/Team27_Android/pull/10)
    - 해결하기 위해 원격 브랜치의 변경 사항 반영
    
    * Revert "fix: 원격 브랜치의 변경 사항 반영"
    
    This reverts commit 06e14e1867735c2abe619dc380e35d16f112dd8e.
    
    * #16 feat: 더미 데이터 생성
    
    * feat : 닉네임 설정창 추가
    
    * feat : 산책 화면 xml 추가
    
    * feat : 주기적으로 위치정보 계산기능 추가
    
    * feat : 산책 시작 버튼 구현
    
    * #16 feat: 실종 동물 목록 표시
    
    - 현재 더미 데이터를 저장하는 LiveData가 업데이트 되기 전에 UI가 먼저 구성되는 문제 발생
    
    * docs: 5주차 구현할 기능 목록
    
    * #16 refactor: StateFlow, awaitAll() 사용하여 더미 데이터 생성
    
    - dce252429e6106afc0240cd2372e5ae1ad9d9d1e 에서 발생한 문제 '현재 더미 데이터를 저장하는 LiveData가 업데이트 되기 전에 UI가 먼저 구성되는 문제 발생' 해결을 위한 시도
    - 코드 리뷰를 위해 우선 commit함
    
    * #16 refactor: TextView 내용을 문자열 리소스로 변경
    
    * #16 refactor: 불필요한 주석 삭제
    
    * docs: 5주차 코드 리뷰 질문 작성
    
    ---------
    
    Co-authored-by: yb0x00 <yeongbeen0x00@gmail.com>
    Co-authored-by: Lee Hyungi <63250720+geist9110@users.noreply.github.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,6 +24,7 @@ android {
         manifestPlaceholders["KAKAO_NATIVE_APP_KEY"] = gradleLocalProperties(rootDir, providers).getProperty("KAKAO_NATIVE_APP_KEY")
     }
 
+
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -46,9 +47,10 @@ android {
         viewBinding = true
         buildConfig = true
     }
-}
 
+}
 dependencies {
+    val roomVersion = "2.6.1"
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
@@ -59,6 +61,8 @@ dependencies {
     implementation("androidx.datastore:datastore-preferences:1.0.0")
     implementation(libs.androidx.activity)
     implementation("com.kakao.sdk:v2-user:2.20.6")
+    implementation("com.kakao.maps.open:android:2.11.9")
+    implementation("com.google.android.gms:play-services-location:21.3.0")
     implementation("com.google.dagger:hilt-android:2.48.1")
     implementation("androidx.navigation:navigation-fragment-ktx:2.8.1")
     implementation("androidx.navigation:navigation-ui-ktx:2.8.1")
@@ -67,9 +71,12 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("com.google.dagger:hilt-android:2.48.1")
     kapt("com.google.dagger:hilt-compiler:2.48.1")
+    implementation("androidx.room:room-ktx:$roomVersion")
     implementation("androidx.activity:activity-ktx:1.9.2")
     implementation("androidx.fragment:fragment-ktx:1.8.3")
-    implementation("androidx.room:room-runtime:2.6.1")
-    annotationProcessor("androidx.room:room-compiler:2.6.1")
+    implementation("androidx.room:room-runtime:$roomVersion")
+    kapt("androidx.room:room-compiler:$roomVersion")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.9")
+    implementation("com.github.bumptech.glide:glide:4.16.0")    //Glide : url -> 이미지 로딩
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
 }
---
+++ app/src/main/java/com/example/togetherpet/home/view/HomeFragment.kt
@@ -0,0 +1,77 @@
+package com.example.togetherpet.home.view
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.togetherpet.databinding.FragmentHomeBinding
+import com.example.togetherpet.home.viewModel.HomeViewModel
+import com.example.togetherpet.adapter.PetListAdapter
+import com.example.togetherpet.testData.viewModel.MissingViewModel
+import com.example.togetherpet.testData.viewModel.UserViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.launch
+
+@AndroidEntryPoint
+class HomeFragment : Fragment() {
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+    private val userViewModel: UserViewModel by viewModels()
+    private val missingViewModel: MissingViewModel by viewModels()
+    private val homeViewModel: HomeViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.homeMissingPetList.layoutManager =
+            LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            val userJob = async { userViewModel.addDummyUser() }
+            val missingJob = async { missingViewModel.addDummyMissingPet() }
+
+            //더미 데이터 생성이 완료될 때까지 기다림
+            awaitAll(userJob, missingJob)
+
+            homeViewModel.loadData()
+
+            homeViewModel.isDataLoaded.collect { isLoaded ->
+                if (isLoaded) {
+                    // 데이터 로드가 완료된 후에만 관찰을 시작
+                    homeViewModel.missingPets.collect { missingInfo ->
+                        Log.d("yeong", "Missing data: $missingInfo")
+
+                        if (missingInfo.isNotEmpty()) {
+                            binding.homeMissingPetList.adapter = PetListAdapter(requireContext(),missingInfo)
+                            binding.homeLogo.visibility = View.GONE
+                            binding.homeSos.visibility = View.VISIBLE
+                        } else {
+                            binding.homeSos.visibility = View.GONE
+                            binding.homeLogo.visibility = View.VISIBLE
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
